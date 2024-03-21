fb16e49d4c226b400cb32b17fface39b6dedb6e1
Mohamed Galdi <contact.galdi@gmail.com>
2024-03-21 03:31:58 +0000

    migrations

---
+++ database/migrations/2024_03_20_224256_create_trips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('area_id')->nullable()->constrained()->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('university_id')->nullable()->constrained()->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('bus_id')->nullable()->constrained()->onDelete('set null')->onUpdate('cascade');
+            $table->integer('times_per_day')->default(1);
+            $table->string('first_going_time')->nullable();
+            $table->string('first_return_time')->nullable();
+            $table->string('second_going_time')->nullable();
+            $table->string('second_return_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
---
+++ database/migrations/2024_03_20_231025_create_trip_rating_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trip_rating', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('trip_id')->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('student_id')->nullable()->constrained()->onDelete('set null')->onUpdate('cascade');
+            $table->integer('bus_rate')->nullable();
+            $table->string('bus_rate_description')->nullable();
+            $table->integer('driver_rate')->nullable();
+            $table->string('driver_rate_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    { 
+        Schema::dropIfExists('trip_rating');
+    }
+};
